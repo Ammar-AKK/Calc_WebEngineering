c1842cffccf0a77d976a8bceb5753d63fda86daf
Ammar_AKK <tu4200113@taibahu.edu.sa>
2023-09-28 12:26:37 +0300

    AddedConflictString

--- src/calc/Calc.java
+++ src/calc/Calc.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main (String[] AKK){
-        System.out.println("WEB ENGINEERING - AMMAR K KHAN - 4200113")
+        System.out.println("WEB ENGINEERING - AMMAR K KHAN - 4200113 - Conflict")
         Scanner Input = new Scanner (System.in); 
         System.out.print("Enter your Age:");
         int Age = Input.nextInt();
