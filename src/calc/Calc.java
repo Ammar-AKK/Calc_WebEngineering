262648a52f8a3db3947ee2d898bfe9b9367d1186
Ammar_AKK <tu4200113@taibahu.edu.sa>
2023-09-28 12:39:56 +0300

    WithConflict

--- src/calc/Calc.java
+++ src/calc/Calc.java
@@ -4,13 +4,11 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main (String[] AKK){
-        System.out.println("WEB ENGINEERING - AMMAR K KHAN - 4200113 - Conflict")
+        System.out.println("WEB ENGINEERING - AMMAR K KHAN - 4200113 - With Conflict")
         Scanner Input = new Scanner (System.in); 
-        System.out.print("Enter your Age:");
+        System.out.print("Enter your Age:")
         int Age = Input.nextInt();
         System.out.println("Age= " + Age);
-        System.out.print("Enter your Year of Birth:");
-        int Year = Input.nextInt();
         System.out.print("Enter your GPA:");
         double GPA = Input.nextDouble();
         System.out.println("GPA= " + GPA);
