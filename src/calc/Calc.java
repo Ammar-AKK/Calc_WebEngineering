db7512ff15945f35483cb964c7866893bc0b7f7e
Ammar_AKK <tu4200113@taibahu.edu.sa>
2023-09-28 11:32:37 +0300

    Course-Name-Added

--- src/calc/Calc.java
+++ src/calc/Calc.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main (String[] AKK){
+        System.out.println("WEB ENGINEERING - AMMAR K KHAN - 4200113")
         Scanner Input = new Scanner (System.in); 
         System.out.print("Enter your Age:");
         int Age = Input.nextInt();
